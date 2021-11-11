88786d7b39bb09ee610e069463412039f3a487b1
Justyna KUJAWA <justy.kujawa@gmail.com>
2021-11-11 19:52:52 +0100

    blogArticle

--- JS/script.js
+++ JS/script.js
@@ -1,37 +1,35 @@
-document.getElementById('test-button').addEventListener('click', function(){
-
-const titleClickHandler = function(event){
+  'use strict';
+  const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
   console.log('Link was clicked!');
   
-  
   /*[DONE]  remove class 'active' from all article links  */
-const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
+  for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
   /* add class 'active' to the clicked link */
-//   console.log('clickedElement:', clickedElement);
+  // console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
-const activeArticles = document.querySelectorAll('article.active');
-
-for(let activeArticle of activeArticles){
+  const activeArticles = document.querySelectorAll('article.active');
+  for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
   /* get 'href' attribute from the clicked link */
+  let elementAttribute = clickedElement.getAttribute("href");
+  console.log(elementAttribute);  
 
   /* find the correct article using the selector (value of 'href' attribute) */
-
+  const targetArticle = document.querySelector(elementAttribute);
+  console.log(targetArticle);
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
-
-const links = document.querySelectorAll('.titles a');
-
-for(let link of links){
-    
-  link.addEventListener('click', titleClickHandler());
-}
-});
+  const links = document.querySelectorAll('.titles a');
+  for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+};
