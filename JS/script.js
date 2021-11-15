faf2a93a7f5c9ff62b79d919c9c213421fc307e8
Justyna KUJAWA <justy.kujawa@gmail.com>
2021-11-15 21:15:43 +0100

    listArticles

--- JS/script.js
+++ JS/script.js
@@ -16,9 +16,9 @@
 
   /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('article.active');
-  for(let activeArticle of activeArticles){
+    for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
-  }
+    }
   /* get 'href' attribute from the clicked link */
   let elementAttribute = clickedElement.getAttribute("href");
   console.log(elementAttribute);  
@@ -26,10 +26,42 @@
   /* find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(elementAttribute);
   console.log(targetArticle);
+
   /* add class 'active' to the correct article */
   targetArticle.classList.add('active');
-  }
+  } 
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+  function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML= '';
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+    for(let article of articles){
+
+  /* get the article id */
+  const articleId = article.getAttribute('id');
+
+  /* find the title element */ /* get the title from the title element */
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    
+  /* create HTML of the link */
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+  console.log(linkHTML);
+
+  /* insert link into titleList */
+  html = html + linkHTML;
+    }
+  titleList.innerHTML = html;
+
   const links = document.querySelectorAll('.titles a');
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  };
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+    }
+  }
+  generateTitleLinks();
